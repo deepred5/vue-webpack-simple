b01abc10c9ac254c09b440275c92785a517feff3
deepred5 <948931744@qq.com>
2018-01-10 19:07:09 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <div id="app">
     <h1>{{ msg }}</h1>
+    <img src="./img/logo.png">
+    <input type="text" v-model="msg">
   </div>
 </template>
 
 <script>
+
+import getData from './util';
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App2'
+      msg: 'Welcome to Your Vue.js'
+    }
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+     async fetchData() {
+      const data = await getData();
+      this.msg = data;
     }
   }
 }
@@ -17,11 +31,10 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
 
   h1 {
     color: green;
   }
 }
-
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -76,3 +76,18 @@ module.exports = {
 
     }
 };
+
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"production"'
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin(),
+    ])
+  }
+  
